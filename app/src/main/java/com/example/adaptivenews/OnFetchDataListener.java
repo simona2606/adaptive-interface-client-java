0c662696ef836c3e2be8698d322b69c7e77a55a7
simona2606 <simonaettari26@gmail.com>
2022-12-05 14:44:07 +0100

    first commit

---
+++ app/src/main/java/com/example/adaptivenews/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.adaptivenews;
+
+import com.example.adaptivenews.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/adaptivenews/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.adaptivenews;
+
+import com.example.adaptivenews.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
